9359c4bb2b46b835999f51eab8536e914de0b214
ftyss <1251898775@qq.com>
2020-04-13 12:00:19 +0000

    显示首页

---
+++ app/Http/Controllers/Index/IndexController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Index;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    //首页
+    public function index()
+    {
+        return view('Index.index');
+    }
+
+    public function login()
+    {
+
+    }
+}
